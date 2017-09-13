1a184fcd18b4effb83cdb534e4b4407a64f93417
Pascal Birchler <hello@pascalbirchler.ch>
2017-09-13 15:02:21 +0200

    Support invalidating individual files
    See #7.

--- inc/CLI_Command.php
+++ inc/CLI_Command.php
@@ -2,6 +2,8 @@
 
 namespace Required\ClearOpcache;
 
+use WP_CLI;
+
 class CLI_Command extends \WP_CLI_Command {
 	/**
 	 * Clear the OPcache for the current WordPress site.
@@ -11,13 +13,11 @@ class CLI_Command extends \WP_CLI_Command {
 	 *     # Clear the OPcache for the website.
 	 *     $ wp opcache clear
 	 */
-	public function __invoke( array $args, array $assoc_args ): void {
-		$nonce = wp_create_nonce( 'clear-opcache' );
-
+	public function clear(): void {
 		$response = wp_remote_post(
 			add_query_arg( [
 				'opcache_action' => 'clear-opcache',
-				'opcache_nonce'  => $nonce,
+				'opcache_nonce'  => wp_create_nonce( 'clear-opcache' ),
 			], home_url( '/' ) ),
 			[
 				'sslverify' => false,
@@ -25,26 +25,77 @@ class CLI_Command extends \WP_CLI_Command {
 		);
 
 		if ( is_wp_error( $response ) ) {
-			/* @var \WP_Error $response */
-			\WP_CLI::error( 'There was an error clearing the OPcache: ' . $response->get_error_message() );
+			WP_CLI::error( 'There was an error clearing the OPcache: ' . $response->get_error_message() );
 		}
 
 		$status = wp_remote_retrieve_response_code( $response );
 
 		if ( 401 === $status ) {
-			\WP_CLI::error( 'It seems like your site requires some sort of authentication. Please allow your server\'s IP address to bypass authentication.' );
+			WP_CLI::error( 'It seems like your site requires some sort of authentication. Please allow your server\'s IP address to bypass authentication.' );
 		}
 
 		if ( 400 === $status ) {
-			\WP_CLI::error( 'There was an unknown error clearing the OPcache. Maybe OPcache is disabled?' );
+			WP_CLI::error( 'There was an unknown error clearing the OPcache. Maybe OPcache is disabled?' );
 		}
 
 		if ( 202 === $status ) {
-			\WP_CLI::success( 'The OPcache was successfully cleared!' );
+			WP_CLI::success( 'The OPcache was successfully cleared!' );
 
 			return;
 		}
 
-		\WP_CLI::error( 'There was an unknown error clearing the OPcache.' );
+		WP_CLI::error( 'There was an unknown error clearing the OPcache.' );
+	}
+
+	/**
+	 * Invalidates a cached script for the current WordPress site.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <script>
+	 * : The path to the script being invalidated.
+	 *
+	 * [--force]
+	 * : Force invalidation, even if not necessary.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # Invalidate a cached script.
+	 *     $ wp opcache invalidate foo/bar.php
+	 */
+	public function invalidate( array $args, array $assoc_args ): void {
+		$response = wp_remote_post(
+			add_query_arg( [
+				'opcache_action' => 'clear-opcache',
+				'opcache_script' => $args['script'],
+				'opcache_force'  => $assoc_args['force'],
+				'opcache_nonce'  => wp_create_nonce( 'clear-opcache' ),
+			], home_url( '/' ) ),
+			[
+				'sslverify' => false,
+			]
+		);
+
+		if ( is_wp_error( $response ) ) {
+			WP_CLI::error( 'There was an error clearing the OPcache: ' . $response->get_error_message() );
+		}
+
+		$status = wp_remote_retrieve_response_code( $response );
+
+		if ( 401 === $status ) {
+			WP_CLI::error( 'It seems like your site requires some sort of authentication. Please allow your server\'s IP address to bypass authentication.' );
+		}
+
+		if ( 400 === $status ) {
+			WP_CLI::error( 'There was an unknown error clearing the OPcache. Maybe OPcache is disabled?' );
+		}
+
+		if ( 202 === $status ) {
+			WP_CLI::success( 'The OPcache was successfully cleared!' );
+
+			return;
+		}
+
+		WP_CLI::error( 'There was an unknown error invalidating the OPcache.' );
 	}
 }
